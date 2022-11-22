9730ccdbe8b88345e227b5c572a1bdc8a716b24a
ionajosephine <ionajosephine@gmail.com>
2022-11-22 16:57:25 +0100

    Customise seed-table.js

--- db/scripts/seed-table.js
+++ db/scripts/seed-table.js
@@ -1,9 +1,9 @@
-import { seedUsersTable } from "../helpers.js";
+import { seedBootcampersTable } from "../helpers.js";
 import { pool } from "../index.js";
 
 try {
-  await seedUsersTable();
-  console.log("Seeded 'users' table");
+  await seedBootcampersTable();
+  console.log("Seeded 'bootcampers' table");
 } catch (err) {
   console.error(err);
 } finally {
