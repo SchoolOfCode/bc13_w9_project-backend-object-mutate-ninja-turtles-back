b29e346c3adde0c378366f13be4052ad6d1637b4
Andrew <112508682+AndyRoo0@users.noreply.github.com>
2022-11-23 09:50:53 +0000

    Revert "Copied files from workshop on API testing with supertest as templates…"

--- db/seed-data.js
+++
@@ -1,9 +0,0 @@
-export const seedData = [
-  { name: "James" },
-  { name: "Mary" },
-  { name: "Robert" },
-  { name: "Patricia" },
-  { name: "John" },
-  { name: "Jennifer" },
-  { name: "Michael" },
-];
